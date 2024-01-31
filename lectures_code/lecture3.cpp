c483469ce9e84e34bc52ac7ef954a795bc2860a5
Ahmed Abdelgawad (ahmedAgawad) <ahmed.m.elshennawi@gmail.com>
2024-01-31 23:48:48 +0200

    lecture 3 done

---
+++ lectures_code/lecture3.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+using namespace std;
+
+// power function example
+/* int raiseToPower(int base, int exponent)
+{
+	int result = 1;
+	for (int i = 0; i < exponent; i++)
+	{
+		result *= base;
+	}
+	return result;
+}
+ */
+
+// implementing swap fn
+/* void swap(int &a, int &b)
+{
+	int tmp = a;
+	a = b;
+	b = tmp;
+} */
+
+int main()
+{
+	/* cout << raiseToPower(3, 4) << endl;
+	cout << raiseToPower(5, 2); */
+
+	/* int q = 3, r = 5;
+	cout << "q " << q << endl;
+	cout << "r " << r << endl;
+	swap(q, r);
+	cout << "q " << q << endl;
+	cout << "r " << r << endl;
+	return 0; */
+}
